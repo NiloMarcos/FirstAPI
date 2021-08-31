8b47aa81d14a83f99526bb384705a78efc70c428
Nilo Marcos <nilo.n2m15@gmail.com>
2021-08-31 18:19:52 -0300

    First API

---
+++ src/mocks/users.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Nilo'
+  },
+  {
+    id: 2,
+    name: 'Fernando'
+  },
+  {
+    id: 3,
+    name: 'Mirela'
+  },
+  {
+    id: 4,
+    name: 'Fernandin'
+  },
+  {
+    id: 5,
+    name: 'Romarin'
+  },
+  {
+    id: 6,
+    name: 'Pernabuquin'
+  },
+];
